5b28afa989fa7bcf76a3a8b4c0649ba3232d6ea2
Yuri <Yuribtr@users.noreply.github.com>
2020-10-17 22:02:26 +0300

    Initial commit

---
+++ ImageSaver.py
@@ -0,0 +1,236 @@
+import time
+import pathlib as pl
+from VkClient import VkClient
+from YaUploader import YaUploader
+import json
+
+
+class ImageSaver:
+    def __init__(self, token_vk: str, token_ya: str, uid_vk, debug_mode=False):
+        self.__debug_mode = debug_mode
+        self.log('\nCreating ImageSaver...', True)
+        self.__client = VkClient(token_vk, uid_vk, debug_mode=debug_mode)
+        self.__uploader = YaUploader(token_ya, debug_mode=debug_mode)
+        self.__types = {'s': 1, 'm': 2, 'x': 3, 'o': 4, 'p': 5, 'q': 6, 'r': 7, 'y': 8, 'z': 9, 'w': 10}
+        if self.__client.is_initialized() and self.__uploader.is_initialized():
+            self.__status = f'{type(self).__name__} initialised.'
+            self.__initialized = True
+        else:
+            self.__status = f'{type(self).__name__} init failed.'
+            self.__initialized = False
+        self.log(self.__status, True)
+
+    @staticmethod
+    def get_auth_link(app_id: str, scope='status'):
+        return VkClient.get_auth_link(app_id=app_id, scope=scope)
+
+    def is_initialized(self):
+        return self.__initialized
+
+    def is_client_initialized(self):
+        return self.__client.is_initialized()
+
+    def is_uploader_initialized(self):
+        return self.__uploader.is_initialized()
+
+    def log(self, message, is_debug_msg=False, sep=' '):
+        if self.__debug_mode or (not self.__debug_mode and not is_debug_msg):
+            if type(message) in [list, dict, tuple, set]:
+                print(*message, sep=sep)
+            else:
+                print(message, sep=sep)
+
+    def get_images_links(self, vk_id=None, album_id='profile'):
+        """
+        Gets user photos for specified user and album
+        :param vk_id: VK user ID, otherwise will be taken user ID during class instance init
+        :param album_id: one of album type: wall, profile, saved
+        :return: list with sublists ['likes': int, 'img url': str, 'extension': str, 'size type': str] or empty list
+        """
+        result = []
+        if not self.__initialized:
+            self.log('Error: not initialized.', True)
+            return result
+        images = []
+        offset = 0
+        count = 100
+        self.log(f'\nRequesting max {count} {album_id} images links from VK {album_id}...', True)
+        while True:
+            user_photos = self.__client.get_user_photos(user_id=vk_id, album_id=album_id, count=count, offset=offset)
+            if not user_photos['success']:
+                self.log(f'Loading image links failed: {user_photos["message"]}', True)
+                break
+            items_count = len(user_photos['object']['items'])
+            self.log(f'Loaded {items_count} images links from VK', True)
+            # if we reached the end
+            if items_count == 0:
+                break
+            images += user_photos['object']['items']
+            offset += count
+            # if returned less items than requested, suppose that we reached the end
+            if items_count < count:
+                break
+            # prevent ban from service
+            time.sleep(0.3)
+        self.log(f'Loading images links finished', True)
+        for item in images:
+            # let's detect images with the maximum resolution, based on dimensions or on type if dimensions is absent
+            img_url = ''
+            img_url_fallback = ''
+            # this needed as fallback if all resolutions will be zero
+            max_type = -1
+            size_type_letter = ''
+            # even if we will be unable to detect max resolution link, we will always take the first one
+            max_res = -1
+            for size in item['sizes']:
+                res = size['height'] * size['width']
+                if max_res < res:
+                    max_res = res
+                    img_url = size['url']
+                # fallback only
+                size_type = self.__types.get(size['type'], 0)
+                if max_type < size_type:
+                    max_type = size_type
+                    size_type_letter = size['type']
+                    # below line needed to prevent additional loop through sizes list in case of fallback
+                    img_url_fallback = size['url']
+            # fallback if unable to detect resolutions
+            # for images older than 2012 year https://vk.com/dev/objects/photo_sizes
+            if max_res == 0:
+                img_url = img_url_fallback
+            # save likes, img url, file extension and size type in sublist
+            result.append([item['likes']['count'], pl.Path(img_url).suffix, img_url, size_type_letter])
+        return result
+
+    def create_folder(self, folder_name: str):
+        """
+        Create folder at Yandex Disk
+        :param folder_name: folder name to be created
+        :return: {'object': 'contains JSON object or None if response body empty',
+                 'success': 'True if requested path found (if specified) and no error codes',
+                 'message': 'contains error string if any or empty string'}
+        """
+        result = {'object': None, 'success': False, 'message': ''}
+        if not self.__initialized:
+            self.log('Error: not initialized', True)
+            result['message'] = 'Not initialized'
+            return result
+        result = self.__uploader.create_folder(folder_name)
+        if result['success']:
+            self.log(f'\nFolder created: {result["object"]["href"]}', True)
+        else:
+            self.log(f'\nError creating folder. {result["message"]}', True)
+        return result
+
+    def upload_remote_files(self, folder: str, files: list, log_file_path: str = None):
+        """
+        Uploads files to Yandex disk using url link
+        :param log_file_path:
+        :param folder: file name with extension on disk where remote file to be stored
+        :param files: list of remote files as sublists ['likes':int, 'img url':str, 'extension':str, 'size type':str]
+        :return: {'object': 'contains JSON object or None if response body empty',
+                 'success': 'True if requested path found (if specified) and no error codes',
+                 'message': 'contains error string if any or empty string'}
+        """
+        result = {'object': [], 'success': False, 'message': ''}
+        if not self.__initialized:
+            self.log('Error: not initialized', True)
+            result['message'] = 'Not initialized'
+            return result
+        self.log(f'\nStart to upload remote files to folder {folder}...', True)
+        if not log_file_path:
+            log_file_path = 'images_log.json'
+        with open(log_file_path, 'w+') as log_file:
+            log = []
+            # limit files to max 100 to prevent unnecessary overloading in demo mode
+            for file in files[:4]:
+                response = self.__uploader.upload_remote_file(folder + '/' + str(file[0]) + file[1], file[2])
+                if response['success']:
+                    self.log(f'Uploading file accepted: {response["object"]["href"]}', True)
+                    log.append({'filename': f'{str(file[0]) + file[1]}', 'size': f'{file[3]}'})
+                else:
+                    self.log(f'Uploading file failed: {file[2]} ({response["object"]})', True)
+                    result = False
+                    break
+                # prevent ban
+                time.sleep(0.3)
+            json.dump(log, log_file)
+            self.log(f'\nLog file saved to {log_file_path}', True)
+        self.log(f'Uploading log file to disk with overwrite...', True)
+        response = self.__uploader.upload_local_file(log_file_path)
+        if response['success']:
+            self.log(f'Log file uploaded to disk', True)
+        else:
+            self.log(f'Uploading log file error. {response["message"]}', True)
+        return result
+
+    def list_disk(self):
+        """
+        This method show files list at Yandex disk, where limit is for pagination purposes
+        Description here: https://yandex.ru/dev/disk/api/reference/all-files.html
+        :return: {'object': 'contains JSON object or None if response body empty',
+                 'success': 'True if requested path found (if specified) and no error codes',
+                 'message': 'contains error string if any or empty string'}
+        """
+        if not self.__initialized:
+            self.log('\nError: not initialized.', True)
+            return False
+        self.log('\nPrinting files list on Yandex disk...', True)
+        result = self.__uploader.list_files(50)['object']
+        self.log(result, True, sep='\n')
+        return result
+
+    def get_user_vk_status(self, user_id=None):
+        """
+        Get VK user status and return as text or return message string
+        :param user_id: ID of VK user, if None - class instance ID will be taken
+        :return: string contained status or empty string if error
+        """
+        if not self.__initialized:
+            self.log('\nError: not initialized.', True)
+            return ''
+        result = self.__client.get_user_status(user_id)
+        if result['success']:
+            self.log(f'\nVK user status: {result["object"]}', True)
+            return result["object"]
+        else:
+            self.log(f'\nVK user status: {result["message"]}', True)
+            return ''
+
+    def delete_file(self, file_path: str):
+        """
+        Delete file from Yandex disk
+        :param file_path: file or folder name
+        :return: {'object': 'contains JSON object or None if response body empty',
+                 'success': 'True if requested path found (if specified) and no error codes',
+                 'message': 'contains error string if any or empty string'}
+        """
+        if not self.__initialized:
+            self.log('\nError: not initialized.', True)
+            return ''
+        self.log('\nTry to delete file or folder: ' + file_path, True)
+        result = self.__uploader.delete_file(file_path)
+        if result['success']:
+            self.log(f'File/Folder deleted: {file_path}', True)
+        else:
+            self.log(f'File/Folder delete error: {result["message"]}', True)
+        return result
+
+    def get_file_info(self, file_path: str):
+        """
+        Get file info on Yandex disk, suitable for check file existence
+        :param file_path: File of folder name on Yandex disk
+        :return: {'object': 'contains JSON object or None if response body empty',
+                 'success': 'True if requested path found (if specified) and no error codes',
+                 'message': 'contains error string if any or empty string'}
+        """
+        if not self.__initialized:
+            self.log('\nError: not initialized', True)
+            return {'object': [], 'success': False, 'message': 'Error: not initialized'}
+        self.log(f'\nChecking if file/folder "{file_path}" is exist...', True)
+        result = self.__uploader.get_file_info(file_path)
+        if result['success']:
+            self.log(f'File/Folder present: {file_path}', True)
+        else:
+            self.log(f'File/Folder not found: {result["message"]}', True)
+        return result
